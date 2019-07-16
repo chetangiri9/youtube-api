7cab5468caad210ef5249ef88bece4b98fe75382
“chetangiri9” <chetangiri9@gmail.com>
2019-07-16 14:44:37 +0530

    Updates

---
+++ precache-manifest.856bf3f2d220b2b37d6ec5f7887a8b65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51da3b3c192af6243e8500f0cb7b3781",
+    "url": "/youtube-api/index.html"
+  },
+  {
+    "revision": "a8fc7b1f43d4641abc15",
+    "url": "/youtube-api/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/youtube-api/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "a8fc7b1f43d4641abc15",
+    "url": "/youtube-api/static/js/main.0cd9bc0e.chunk.js"
+  },
+  {
+    "revision": "005f278b5305a7aab350",
+    "url": "/youtube-api/static/js/runtime~main.9f8da7b4.js"
+  }
+]);
